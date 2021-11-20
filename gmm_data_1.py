f766eabfc9f8ee357adaef788f17c457200954af
Vaidehi <vaidehiatpadkar1@gmail.com>
2021-11-20 11:42:29 +0530

    Add function to initialize mean vectors, covariance matrices and mixture coefficients

--- gmm_data_1.py
+++ gmm_data_1.py
@@ -23,3 +23,26 @@ for line in data_1_file2:
 data_1 = []
 data_1.extend(data_1_class1)
 data_1.extend(data_1_class2)
+
+
+def Initialization(data, k, dim):
+    mean_vecs = []
+    mean_indices = []
+    while (len(mean_vecs)<k):
+        index = random.randint(0, len(data)-1)
+        if index not in mean_indices:
+            mean_vecs.append(data[index])
+            mean_indices.append(index)
+        else:
+            continue
+    
+    cov_matrices = []
+    for i in range(k):
+        matrix = np.identity(dim)
+        cov_matrices.append(matrix)
+    
+    mixture_coefficients = [(1/k) for i in range(k)]
+    
+    return mean_vecs, cov_matrices, mixture_coefficients
+
+mean, cov, mix = Initialization(data_1, 2, 2)
