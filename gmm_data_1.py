5141b6f0263ccf184b5ace1085f1c9ff5e808acd
Vaidehi <vaidehiatpadkar1@gmail.com>
2021-11-22 19:59:25 +0530

    Rename prior to posterior

--- gmm_data_1.py
+++ gmm_data_1.py
@@ -72,7 +72,6 @@ def gaussian_density(x, mean, cov, dim):
 
 
 def LogLikelihood(data, k, mean_vecs, cov_matrices, mix_coef, dim):
-    
     log_likelihood = 0
     
     for i in range(len(data)):
@@ -83,10 +82,8 @@ def LogLikelihood(data, k, mean_vecs, cov_matrices, mix_coef, dim):
 
     return log_likelihood
 
-#mean, cov, mix_coef = Initialization(data_1, 2, 2)
-#print("log likelihood for first iteration = ", LogLikelihood(data_1, 2, mean, cov, mix_coef, 2))
 
-def Prior_prob(k, k_val, x, mean_vecs, cov_matrs, mix_coef, dim):
+def Posterior_prob(k, k_val, x, mean_vecs, cov_matrs, mix_coef, dim):
     evidence = 0
     for i in range(k):
         evidence += mix_coef[i]*gaussian_density(x, mean_vecs[i], cov_matrs[i], dim)
@@ -101,10 +98,10 @@ def GMM(data, test_data, test_data_true_class, k, dim):
     def no_of_points_assigned(mean_vecs, cov_matrs, mix_coef):
         assigned_k = []
         for i in range(len(data)):
-            prior = Prior_prob(k, 0, data[i], mean_vecs, cov_matrs, mix_coef, dim)
+            prior = Posterior_prob(k, 0, data[i], mean_vecs, cov_matrs, mix_coef, dim)
             assigned_k_val=0
             for j in range(1,k):
-                temp = Prior_prob(k, j, data[i], mean_vecs, cov_matrs, mix_coef, dim)
+                temp = Posterior_prob(k, j, data[i], mean_vecs, cov_matrs, mix_coef, dim)
                 if temp>prior:
                     prior=temp
                     assigned_k_val=j
@@ -122,7 +119,7 @@ def GMM(data, test_data, test_data_true_class, k, dim):
         for i in range(k):
             mean_vec_i = np.zeros(dim)
             for j in data:
-                mean_vec_i += Prior_prob(k, i, j, curr_means, curr_covs, curr_mix, dim)*np.array(j)
+                mean_vec_i += Posterior_prob(k, i, j, curr_means, curr_covs, curr_mix, dim)*np.array(j)
             mean_vec_i = mean_vec_i/no_of_points[i]
             means.append(mean_vec_i)
              
@@ -137,7 +134,7 @@ def GMM(data, test_data, test_data_true_class, k, dim):
             for j in range(len(data)):
                 x = np.array(data[j])
                 mean_i = np.array(curr_means[i])
-                cov_vec_i += Prior_prob(k, i, x, curr_means, curr_covs, curr_mix, dim)*(x-mean_i).reshape((dim,1))*(x-mean_i).reshape((1,dim))
+                cov_vec_i += Posterior_prob(k, i, x, curr_means, curr_covs, curr_mix, dim)*(x-mean_i).reshape((dim,1))*(x-mean_i).reshape((1,dim))
             cov_vec_i = cov_vec_i/no_of_points[i]
             covs.append(cov_vec_i)
             
@@ -173,31 +170,32 @@ def GMM(data, test_data, test_data_true_class, k, dim):
     plt.suptitle("GMM Clustering on data_1 with K=%d"%(k), fontsize=22)
     plt.subplot(1,2,1)
     plt.title("Original classes", fontsize=18)
-    plt.scatter([x[0] for x in data_1_class1], [x[1] for x in data_1_class1])
-    plt.scatter([x[0] for x in data_1_class2], [x[1] for x in data_1_class2])
+    plt.scatter([x[0] for x in data_1_class1], [x[1] for x in data_1_class1], color="darkblue", label="class 0")
+    plt.scatter([x[0] for x in data_1_class2], [x[1] for x in data_1_class2], color="royalblue", label="class 1")
+    plt.legend()
     
     plt.subplot(1,2,2)
     plt.title("Classes after clustering", fontsize=18)
+    colors = ['teal', 'mediumaquamarine', 'lightgreen', 'yellowgreen', 'slateblue', 'orchid']
     for i in range(k):
         indices = np.where(np.array(data_assignment, dtype=object)==i)
-        plt.scatter([x[0] for x in np.array(X_train)[indices]], [x[1] for x in np.array(X_train)[indices]])
+        plt.scatter([x[0] for x in np.array(X_train)[indices]], [x[1] for x in np.array(X_train)[indices]], color=colors[i], label="class %d"%(i))
         
-    plt.scatter([i[0] for i in curr_means],[i[1] for i in curr_means], color="black")
-    
+    plt.scatter([i[0] for i in curr_means],[i[1] for i in curr_means], color="black", label="centers")
+    plt.legend()
     
     #Code for getting accuracy if k=2
     if k==2:
         assigned_k = [] #predicted classes
         for i in range(len(test_data)):
-            prior = Prior_prob(k, 0, test_data[i], curr_means, curr_covs, curr_mix_coef, dim)
-            assigned_k_val = 0
+            prior = Posterior_prob(k, 0, test_data[i], curr_means, curr_covs, curr_mix_coef, dim)
+            assigned_k_val = 0 
             for j in range(1,k):
-                temp = Prior_prob(k, j, test_data[i], curr_means, curr_covs, curr_mix_coef, dim)
+                temp = Posterior_prob(k, j, test_data[i], curr_means, curr_covs, curr_mix_coef, dim)
                 if temp>prior:
                     prior=temp
                     assigned_k_val=j
             assigned_k.append(assigned_k_val)
-        
         
         confusion_mat = confusion_matrix(test_data_true_class, assigned_k)   
         accuracy = accuracy_score(test_data_true_class, assigned_k)
